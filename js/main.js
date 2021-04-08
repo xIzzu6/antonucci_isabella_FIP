8ae8211137262501a03c06d84d793c97c67e5669
Izzu A <>
2021-04-08 12:33:09 -0400

    trouble with burger menu

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
-console.log("javascript is linked up");
+(function(){
+	"use strict";
+	console.log("fired");
 
+	let button = document.querySelector("#button");
+	let burgerCon = document.querySelector("#burgerCon");
+
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("slideToggle");
+		button.classList.toggle("expanded");
+	}
+
+	button.addEventListener("click", hamburgerMenu);
+
+})();
